88408f7cdb817088cb3f45657dafedcd99395e6e
ArizCefDev <arizcefdev@gmail.com>
2023-06-09 21:45:44 +0400

    Update Project

--- WinForms_9F/Form1.Designer.cs
+++ WinForms_9F/Form1.Designer.cs
@@ -28,79 +28,187 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
             label1 = new Label();
             label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox4 = new TextBox();
+            button4 = new Button();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(232, 325);
-            button1.Name = "button1";
-            button1.Size = new Size(189, 86);
-            button1.TabIndex = 0;
-            button1.Text = "Register";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(219, 119);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(249, 39);
-            textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(219, 222);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(249, 39);
-            textBox2.TabIndex = 2;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(221, 73);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(82, 38);
             label1.Name = "label1";
-            label1.Size = new Size(128, 32);
-            label1.TabIndex = 3;
-            label1.Text = "Username";
+            label1.Size = new Size(53, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Ad:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(221, 174);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(82, 103);
             label2.Name = "label2";
-            label2.Size = new Size(122, 32);
-            label2.TabIndex = 4;
-            label2.Text = "Password";
+            label2.Size = new Size(91, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Vəzifə:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(82, 163);
+            label3.Name = "label3";
+            label3.RightToLeft = RightToLeft.No;
+            label3.Size = new Size(81, 32);
+            label3.TabIndex = 2;
+            label3.Text = "Maaş:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(255, 38);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(256, 39);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(255, 100);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(256, 39);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(255, 160);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(256, 39);
+            textBox3.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(80, 308);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(449, 287);
+            dataGridView1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Lime;
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(891, 38);
+            button1.Name = "button1";
+            button1.Size = new Size(312, 52);
+            button1.TabIndex = 7;
+            button1.Text = "Əlavə et";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Yellow;
+            button2.Location = new Point(891, 100);
+            button2.Name = "button2";
+            button2.Size = new Size(312, 52);
+            button2.TabIndex = 8;
+            button2.Text = "Göstər";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Red;
+            button3.Location = new Point(891, 163);
+            button3.Name = "button3";
+            button3.Size = new Size(312, 52);
+            button3.TabIndex = 9;
+            button3.Text = "Sil";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(717, 428);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(268, 39);
+            textBox4.TabIndex = 10;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(1010, 420);
+            button4.Name = "button4";
+            button4.Size = new Size(125, 47);
+            button4.TabIndex = 11;
+            button4.Text = "Axtar";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(717, 372);
+            label4.Name = "label4";
+            label4.RightToLeft = RightToLeft.No;
+            label4.Size = new Size(125, 32);
+            label4.TabIndex = 12;
+            label4.Text = "İşçi axtar:";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(725, 550);
-            Controls.Add(label2);
-            Controls.Add(label1);
+            BackColor = Color.Teal;
+            ClientSize = new Size(1283, 640);
+            Controls.Add(label4);
+            Controls.Add(button4);
+            Controls.Add(textBox4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
-            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            Margin = new Padding(6, 6, 6, 6);
+            Margin = new Padding(6);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
-        private TextBox textBox1;
-        private TextBox textBox2;
         private Label label1;
         private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox4;
+        private Button button4;
+        private Label label4;
     }
 }
--- WinForms_9F/Form1.cs
+++ WinForms_9F/Form1.cs
@@ -1,4 +1,5 @@
-using Microsoft.Data.SqlClient;
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace WinForms_9F
 {
@@ -9,40 +10,121 @@ namespace WinForms_9F
             InitializeComponent();
         }
 
-        public string Constring = @"Server=WIN-VD08C7OPT8H\SQLEXPRESS; Database=Ado9F;Trusted_Connection=true; Encrypt=false;";
-        private void button1_Click(object sender, EventArgs e)
+        private const string Constring = @"Server=WIN-VD08C7OPT8H\SQLEXPRESS; Database=Ado9F;Trusted_Connection=true; Encrypt=false;";
+        SqlConnection connection = new SqlConnection(Constring);
+
+        private void button1_Click_1(object sender, EventArgs e)
         {
-            //ADO.NET
-            
-
-            SqlConnection connection = new SqlConnection(Constring);
-
+            //Əlavə et
             try
             {
                 connection.Open();
-                MessageBox.Show("Database Open");
 
-                string cmd = @"INSERT INTO Register(Username,UserPassword) 
-                    VALUES(@usern,@userpass)";
+                string cmd = @"INSERT INTO Employee(E_name,E_position,E_salary) 
+                    VALUES(@eName,@ePosition, @eSalary)";
 
                 SqlCommand sqlCommand = new SqlCommand(cmd, connection);
 
-                sqlCommand.Parameters.AddWithValue("@usern", textBox1.Text);
-                sqlCommand.Parameters.AddWithValue("@userpass", textBox2.Text);
+                sqlCommand.Parameters.AddWithValue("@eName", textBox1.Text);
+                sqlCommand.Parameters.AddWithValue("@ePosition", textBox2.Text);
+                sqlCommand.Parameters.AddWithValue("@eSalary", textBox3.Text);
                 sqlCommand.ExecuteNonQuery();
 
-                MessageBox.Show("Daxil edildi");
+                MessageBox.Show("İşçi əlavə olundu!");
+                Show();
             }
             catch (Exception u)
             {
-                MessageBox.Show("Error"+ u.Message);
+                MessageBox.Show("Xəta" + u.Message);
             }
             finally
             {
                 connection.Close();
-                MessageBox.Show("Database Close");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Sil
+            foreach (DataGridViewRow item in dataGridView1.SelectedRows)
+            {
+                int id = Convert.ToInt32(item.Cells[0].Value);
+                Delete(id);
+                MessageBox.Show("İşçi silindi!");
+                Show();
+
+            }
+        }
+
+        //Methodlar
+        private void Show()
+        {
+            //Göstər
+            try
+            {
+                string cmd = @"SELECT * FROM Employee";
+
+                SqlCommand sqlCommand = new SqlCommand(cmd, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+                DataTable data = new DataTable();
+                adapter.Fill(data);
+                dataGridView1.DataSource = data;
+            }
+            catch (Exception u)
+            {
+                MessageBox.Show("Xəta" + u.Message);
             }
 
+        }
+        private void Delete(int id)
+        {
+            //Sil
+            try
+            {
+                connection.Open();
+                string cmd = @"DELETE FROM Employee WHERE id=@id";
+                SqlCommand sqlCommand = new SqlCommand(cmd, connection);
+                sqlCommand.Parameters.AddWithValue("@id", id);
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception u)
+            {
+                MessageBox.Show("Xəta" + u.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Axtar
+            try
+            {
+                connection.Open();
+                string cmd = "SELECT * FROM Employee WHERE E_name=@name";
+                SqlCommand sqlCommand = new SqlCommand(cmd, connection);
+                sqlCommand.Parameters.AddWithValue("@name", textBox4.Text);
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand);
+                DataTable data = new DataTable();
+                adapter.Fill(data);
+                dataGridView1.DataSource = data;
+            }
+            catch (Exception u)
+            {
+                MessageBox.Show("Xəta" + u.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
